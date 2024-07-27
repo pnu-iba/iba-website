a22383e2bb32142387917491267aac45ec53770d
pnu-iba <minchul0123@gmail.com>
2024-07-27 19:27:17 +0900

    article.html 작성 필요

--- src/main/java/com/iba/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/com/iba/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.iba.springbootdeveloper.controller;
 
+import com.iba.springbootdeveloper.domain.Article;
 import com.iba.springbootdeveloper.dto.ArticleListViewResponse;
+import com.iba.springbootdeveloper.dto.ArticleViewResponse;
 import com.iba.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable ("id") Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article (title, content) VALUES ('박준형', '박준형')
-INSERT INTO article (title, content) VALUES ('박민규', '박민규')
-INSERT INTO article (title, content) VALUES ('IBA', '웹사이트 만들기')
+INSERT INTO article (title, content) VALUES ('박준형', '박준형', NOW(), NOW())
+INSERT INTO article (title, content) VALUES ('박민규', '박민규', NOW(), NOW())
+INSERT INTO article (title, content) VALUES ('IBA', '웹사이트 만들기', NOW(), NOW())
