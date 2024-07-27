c60c27447653dadc5f8b564e93872d5251e0da8a
pnu-iba <minchul0123@gmail.com>
2024-07-27 19:31:42 +0900

    sql 오류 해결

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article (title, content) VALUES ('박준형', '박준형', NOW(), NOW())
-INSERT INTO article (title, content) VALUES ('박민규', '박민규', NOW(), NOW())
-INSERT INTO article (title, content) VALUES ('IBA', '웹사이트 만들기', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('박준형', '박준형', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('박민규', '박민규', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('IBA', '웹사이트 만들기', NOW(), NOW())
