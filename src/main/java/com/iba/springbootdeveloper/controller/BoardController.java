2d8d8b06dd8db56ecfc9d50684c36c69028f1ae8
pnu-iba <minchul0123@gmail.com>
2024-07-27 22:37:54 +0900

    article.js 작성 필요

--- src/main/java/com/iba/springbootdeveloper/controller/BoardController.java
+++ src/main/java/com/iba/springbootdeveloper/controller/BoardController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AboutController {
-    @GetMapping("/about")
+public class BoardController {
+    @GetMapping("/board")
     public String about() {
-        return "about";
+        return "boardList";
     }
 }
